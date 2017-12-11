79fc40d269d6effa5aa3fcb172198ac9fa86af8c
Victor Veytsman <vityavv@gmail.com>
2017-12-10 20:49:47 -0500

    Change the special character to the tilda as you realize that the asterisk is for italics and re-evaluate your life choices. Also fix a thing

--- app.js
+++ app.js
@@ -27,21 +27,21 @@ var iogames = [
 var commandset = [
 	"HELP MENU",
 	"===============================================",
-	">> *help: this help screen",
-	">> *fuck: fucks up any text you send it",
-	">> *iogame: gives you a random io game out of 150",
-	">> *question: gives you the response to your question (ex. 5+1, how many ounces in a liter, etc.), courtesy of Wolfram|Alpha"
+	">> ~help: this help screen",
+	">> ~fuck: fucks up any text you send it",
+	">> ~iogame: gives you a random io game out of 150",
+	">> ~question: gives you the response to your question (ex. 5+1, how many ounces in a liter, etc.), courtesy of Wolfram|Alpha"
 ];
 function parseMessage(message) {
 	//Dad bot:
 	if (message.content.substring(0, 3).toLowerCase() == "im ") {
-		message.channel.send("Hi, " + message.content.substring(3) + ", I'm Dad!");
+		message.channel.send("Hi, " + message.content.substring(3) + ", I'm Victor's Random Bot!");
 	}
 	if (message.content.substring(0, 4).toLowerCase() == "i'm ") {
 		message.channel.send("Hi, " + message.content.substring(4) + ", I'm Victor's Random Bot!");
 	}
 	//Fuckitup-bot
-	if (message.content.substring(0, 6).toLowerCase() == "*fuck ") {
+	if (message.content.substring(0, 6).toLowerCase() == "~fuck ") {
 		var text = message.content.substring(5);
 		text = text.split("");
 		for (var i = 0; i < text.length; i++) {
@@ -53,13 +53,13 @@ function parseMessage(message) {
 		}
 		message.channel.send(text.join(""));
 	}
-	if (message.content.substring(0, 7).toLowerCase() == "*iogame") {
+	if (message.content.substring(0, 7).toLowerCase() == "~iogame") {
 		message.channel.send("http://" + iogames[Math.floor(Math.random()*iogames.length)] + ".io");
 	}
-	if (message.content.substring(0, 5).toLowerCase() == "*help") {
+	if (message.content.substring(0, 5).toLowerCase() == "~help") {
 		message.channel.send(commandset.join("\n"));
 	}
-	if (message.content.substring(0, 10).toLowerCase() == "*question ") {
+	if (message.content.substring(0, 10).toLowerCase() == "~question ") {
 		http.get("http://api.wolframalpha.com/v1/result?i="+encodeURIComponent(message.content.substring(10))+"&appid=5VW435-9JY9W6U2P9", function(result) {
 			var body = "";
 			result.on("data", data => {
