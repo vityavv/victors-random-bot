b4b92825ddacc1f569da11de1cb462a9c800df2d
Victor Veytsman <vityavv@gmail.com>
2017-12-11 18:23:15 -0500

    make the code for the ~fonts function a little smaller and switched up the dividers

--- app.js
+++ app.js
@@ -103,9 +103,7 @@ function parseMessage(message) {
 	if (message.content.substring(0, 6).toLowerCase() == "~fonts") {
 		var fontmessage1 = fonts;
 		var fontmessage2 = fontmessage1.splice(0, Math.floor(fontmessage1.length/2));
-		fontmessage1 = fontmessage1.join(" || ");
-		fontmessage2 = fontmessage2.join(" || ");
-		message.channel.send(fontmessage2);
-		message.channel.send(fontmessage1);
+		message.channel.send(fontmessage2.join("`||`"));
+		message.channel.send(fontmessage1.join("`||`"));
 	}
 }
